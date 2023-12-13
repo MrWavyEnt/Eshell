805d288566efc1925d9b045ac87f819addb5bc48
MrWavyEnt <chimacollinsekoma@gmail.com>
2023-12-12 23:49:39 -0800

    cmdexec.c

--- cmdexec.c
+++ cmdexec.c
@@ -1,5 +1,16 @@
 #include "main.h"
 /**
+ * handle_exit - handles the "exit" built-in command.
+ * Prints a farewell message and exits the shell.
+ * Return: NULL
+ */
+void handle_exit(void)
+{
+	printf("Exiting the shell, Goodbye!\n");
+	exit(0);
+}
+
+/**
  * cmdexec - Executes a command specified by the argv array
  * using the execve system call.
  * @argv: Array of strings representing the command and it's arguments
@@ -12,6 +23,11 @@ void cmdexec(char **argv)
 	if (argv)
 	{
 		command = argv[0];
+
+		if (strcmp(command, "exit") == 0)
+		{
+			handle_exit();
+		}
 
 		actual_command = find_location(command);
 
